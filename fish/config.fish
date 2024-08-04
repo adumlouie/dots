d785c68940d1366f87c268b6f58231ee758c36a4
adumlouie <adumyumyum@csu.fullerton.edu>
2024-08-03 22:15:12 -0700

    feat: testie :smile:

--- fish/config.fish
+++ fish/config.fish
@@ -6,4 +6,17 @@ end
 
 alias nv=nvim
 
+eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+
+fzf --fish | source
 starship init fish | source
+
+export FZF_DEFAULT_COMMAND="fd . $HOME"
+export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+export FZF_ALT_C_COMMAND="fd -t d . $HOME"
+
+set -gx NVM_DIR ~/.nvm
+set -gx XDG_CONFIG_HOME ~/.config
+source ~/.config/fish/conf.d/nvm.fish
+
+bind \cz 'fg 2>/dev/null; commandline -f repaint'
